bddafea9d92dce5166fed4bdb04c13897427b500
Bucky Collins <bucky.collins.67@gmail.com>
2022-11-20 17:54:05 -0800

    added daily codewars solution

---
+++ 11-20-2022-youCantCountUnderPressure1.js
@@ -0,0 +1,33 @@
+// -------------------------------------------------------------------- //
+// ------------------------CODE WARS DAILY CHALLENGE------------------- //
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+
+// -------------------------------------------------------------------- //
+// ------------------------------PREP METHOD--------------------------- //
+
+// P - Parameters
+// We will be given an interger
+
+// R - Returns
+// We will be expected to return double that interger
+
+// E - Examples
+//  1  =>  2
+//  2  =>  4
+
+// P - Psuedocode
+// 1.) take the interget and multiply it by two.
+// 2.) return it.
+// 3.) 
+// 4.) 
+
+// -------------------------------------------------------------------- //
+// --------------------------------SOLUTION---------------------------- //
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+  }
