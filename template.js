8d10ab56f566513430ecaadaa9930eccb4b2a341
Bucky Collins <bucky.collins.67@gmail.com>
2022-11-23 10:17:54 -0800

    added template for future use

---
+++ template.js
@@ -0,0 +1,30 @@
+// -------------------------------------------------------------------- //
+// ------------------------CODE WARS DAILY CHALLENGE------------------- //
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// -------------------------------------------------------------------- //
+// ------------------------------PREP METHOD--------------------------- //
+
+// P - Parameters
+// We will be given 
+
+// R - Returns
+// We will be expected to return
+
+// E - Examples
+//    =>
+//    =>
+
+// P - Psuedocode
+// 1.) 
+// 2.) 
+// 3.) 
+// 4.) 
+
+// -------------------------------------------------------------------- //
+// --------------------------------SOLUTION---------------------------- //
