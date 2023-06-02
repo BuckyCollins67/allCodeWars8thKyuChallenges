aec2dbb7b98ab09c8cd5e4f5b7806ebdb07f383d
Bucky Collins <bucky.collins.67@gmail.com>
2023-06-01 20:42:22 -0700

    daily code wars for june 1st.

---
+++ 06-01-2023-convertABooleanToAString.js
@@ -0,0 +1,32 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// PREP Method
+
+// Parameters
+
+// We will be given a boolean value (true or false)  Only valid inputs, so no NaNs or empty values.
+
+// Returns
+
+// We will be expected to return the string to the cooresponding boolean value "true" or "false".
+
+// Examples
+
+// (true, "true")
+// (false, "false")
+
+// Psuedocode
+
+// 1.  If the boolean is true, return "true"
+// 2.  Else, return "false"
+
+function booleanToString(b){
+    if (b === true) {
+        return "true"
+    } else {
+        return "false"
+        
+    }
+  }
