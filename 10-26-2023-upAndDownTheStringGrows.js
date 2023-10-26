f80a99f7cbcc3ac28274d5ae5382c6e98a8574a3
Bucky Collins <bucky.collins.67@gmail.com>
2023-10-26 06:43:55 -0700

    added a daily string based codewars

---
+++ 10-26-2023-upAndDownTheStringGrows.js
@@ -0,0 +1,41 @@
+// -------------------------------------------------------------------- //
+// ------------------------CODE WARS DAILY CHALLENGE------------------- //
+
+// Many programming languages provide the functionality of converting a string to uppercase or lowercase. For example, upcase/downcase in Ruby, upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript, uppercase/lowercase in Kotlin. Typically, these methods won't change the size of the string.
+
+// For example, in Ruby, str.upcase.downcase.size == str.size is true for most cases.
+
+// However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+
+// For example, in Ruby, That means str.upcase.downcase.size > str.size
+
+// You should just set the value of STRANGE_STRING to meet the previous criteria.
+
+// Note: Meta programming is not allowed in this kata. So, the size of your solution is limited.
+
+// -------------------------------------------------------------------- //
+// ------------------------------PREP METHOD--------------------------- //
+
+// P - Parameters
+// We will be given a blank variable.  This is so weird.  
+
+// R - Returns
+// We will be expected to return that variable after converting it to upercase and then to lowercase, and it is expected that the length of the variable has changed.
+
+// E - Examples
+//    =>  None given go fuck yourself
+//    =>
+
+// P - Psuedocode
+// 1.) ...?  I don't even know.  this isn't something that's solved logically.
+// 2.) 
+// 3.) 
+// 4.) 
+
+// -------------------------------------------------------------------- //
+// --------------------------------SOLUTION---------------------------- //
+
+const STRANGE_STRING = "ß"
+
+//This exercise was made to demonstrate how unicode characters can play tricks.  when that particular character goes through the process, it gets changed to "ss" which is longer.
+//Neat lesson, but not a logic lesson.
