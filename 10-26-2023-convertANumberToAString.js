39998cd36c8b6afe85026b2a1278c0f71a1e1335
Bucky Collins <bucky.collins.67@gmail.com>
2023-10-26 22:49:02 -0700

    Added daily string code wars challenge

---
+++ 10-26-2023-convertANumberToAString.js
@@ -0,0 +1,37 @@
+// -------------------------------------------------------------------- //
+// ------------------------CODE WARS DAILY CHALLENGE------------------- //
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// -------------------------------------------------------------------- //
+// ------------------------------PREP METHOD--------------------------- //
+
+// P - Parameters
+// We will be given an interger
+
+// R - Returns
+// We will be expected to return a string
+
+// E - Examples
+//  123  =>  "123"
+//   67 =>  "67"
+
+// P - Psuedocode
+// 1.) Take in the input Interger
+// 2.) Use the .toString method.  This is a method that can be applied to all interger objects.
+// 3.) 
+// 4.) 
+
+// -------------------------------------------------------------------- //
+// --------------------------------SOLUTION---------------------------- //
+
+function numberToString(num) {
+    return num.toString();
+  }
